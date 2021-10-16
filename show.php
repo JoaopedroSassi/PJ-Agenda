22bfc66118fd4f7cb57685fa256e49929acf666c
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-10-16 01:25:29 -0300

    Add bak_btn + include show.php + Style back_btn

--- show.php
+++ show.php
@@ -2,6 +2,7 @@
    include_once('templates/header.php')
 ?>
    <div class="container" id="view-contact-container">
+      <?php include_once('templates/back_btn.php') ?>
       <h1 id="main-title"><?= $contact['name']?></h1>
       <p class="bold">Telefone:</p>
       <p><?= $contact['phone']?></p>
