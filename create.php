65e2fb39411e4f3399049a092b2df1c1c3a940c2
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-10-16 01:39:50 -0300

    Create.php + CSS create.php

--- create.php
+++ create.php
@@ -1,9 +1,26 @@
 <?php
    include_once('templates/header.php')
 ?>
-   
-   <h1>Página create</h1>
-
+   <div class="container">
+      <?php include_once('templates/back_btn.php') ?>
+      <h1 id="main-title">Criar contato</h1>
+      <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+         <input type="hidden" name="type" value="Create">
+         <div class="form-group">
+            <label for="name">Nome do contato:</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Digite o nome" required>
+         </div>
+         <div class="form-group">
+            <label for="phone">Telefone do contato:</label>
+            <input type="text" class="form-control" name="phone" id="phone" placeholder="Digite o telefone" required>
+         </div>
+         <div class="form-group">
+            <label for="observations">Observações:</label>
+            <textarea type="text" class="form-control" name="observations" id="observations" placeholder="Insira as observações" rows="3"></textarea>
+         </div>
+         <button type="submit" id="btn-create" class="btn btn-primary">Cadastrar</button>
+      </form>
+   </div>
 <?php
    include_once('templates/footer.php')
 ?>
